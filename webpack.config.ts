f576a05b150f4e89aa247a2854fe33b63b784728
Mykolai Skydan <skmykolai@gmail.com>
2024-01-17 15:49:29 +0000

    move index.html

--- webpack.config.ts
+++ webpack.config.ts
@@ -23,7 +23,7 @@ export default (env: BuildEnv) => {
 		paths: {
 			entry: path.resolve(__dirname, 'src', 'index.tsx'),
 			output: path.resolve(__dirname, 'build'),
-			html: path.resolve(__dirname, 'public', 'index.html'),
+			html: path.resolve(__dirname, 'index.html'),
 			src: path.resolve(__dirname, 'src'),
 		}
 	}
