fead1d5875d9b8b0529be339afb79aed2c4aa108
Mykolai Skydan <skmykolai@gmail.com>
2024-03-16 18:50:10 +0000

    fix loader

--- src/app/router/ui/Routes.tsx
+++ src/app/router/ui/Routes.tsx
@@ -4,12 +4,13 @@ import { Suspense } from 'react'
 import { Spinner } from 'shared/ui/Spinner'
 import AuthRequired from './AuthRequired'
 import { useTitle } from './SetTitle'
+import styles from './Routes.module.less'
 
 export default function RoutesConfig() {
 
 	useTitle()
 	return (
-		<Suspense fallback={<Spinner />}>
+		<Suspense fallback={<Spinner className={styles.spinner} />}>
 			<Routes>
 				{Object.keys(AppRoutes).map((elem) => <Route key={elem} path={elem} element={
 					<>
